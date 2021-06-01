fdcc5ef979d49ba31fb6b9f3fcc628a600363d0a
MD Ashraful Alam <ashraful9194@gmail.com>
2021-06-01 21:22:45 +0600

    Update LCMSUM - LCM Sum.cpp

--- LCMSUM - LCM Sum.cpp
+++ LCMSUM - LCM Sum.cpp
@@ -38,6 +38,22 @@ void pre_calc(int n)
 }
 int32_t main()
 {
+    /*
+    
+    https://forthright48.com/spoj-lcmsum-lcm-sum/
+    
+    SUM–n=LCM(1,n)+LCM(2,n)+…+LCM(n−1,n)  (eq1)
+SUM–n=LCM(n−1,n)+LCM(n−2,n)+…+LCM(1,n) ( eq2 Add them )
+2(SUM−n)=n^2/gcd(1,n)+n^2/gcd(2,n)+…n^2/gcd(n−1,n)
+2(SUM−n)=∑n^2/gcd(i,n)  -->(i>=1 && i<n)
+2(SUM−n)=n∑n/gcd(i,n)
+2(SUM−n)=n∑d|n,d≠nϕ(nd)×nd  (but, nd is also a divisor of n )
+2(SUM−n)=n∑d|n,d≠1ϕ(d)×d ( when d=1, we get ϕ(1)times1 )///Till now this is enough
+2(SUM−n)=n(∑d|n(ϕ(d)×d)−1)
+2(SUM−n)=n∑d|n(ϕ(d)×d)–n
+2SUM–2n+n=n∑d|nϕ(d)×d
+2SUM–n=n∑d|nϕ(d)×d
+    */
      ios_base::sync_with_stdio(false);
      cin.tie(NULL);
      euler_phi(1000000);
